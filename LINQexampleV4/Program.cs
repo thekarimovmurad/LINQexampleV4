743c45f4da5b4e2e748a2e3c86f168ac23ca87e2
thekarimovmurad <thekarimovmurad@gmail.com>
2024-01-13 04:13:56 +0100

    Add project files.

---
+++ LINQexampleV4/Program.cs
@@ -0,0 +1,46 @@
+﻿using LINQexampleV4;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+List<Employee> employeeList= Data.GetEmployees();
+List<Department> departmentList = Data.GetDepartments();
+
+#region
+  
+#endregion
+#region
+
+#endregion
+#region
+
+#endregion
+#region
+
+#endregion
+#region
+
+#endregion
+#region
+
+#endregion
+#region
+
+#endregion
+#region
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
